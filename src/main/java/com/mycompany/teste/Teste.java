bffc00a066688defe1f2c0746eaa4cd143f99f1f
VictorLScheuerRodrigues <162334303+VictorLSRodrigues@users.noreply.github.com>
2025-11-04 08:41:48 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/teste/Teste.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teste;
+
+/**
+ *
+ * @author 1072410296
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        
+        
+        
+        
+        
+    }
+}
